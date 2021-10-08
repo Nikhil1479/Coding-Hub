216ffebdd8960c8d9309ffe8cc41c238a15c6b6d
Nikhil <nikhil956839@gmail.com>
2021-10-08 23:16:28 +0530

    commit

---
+++ C/Data Structures/Array ADT/Search()/Linear Search()/improved_linear_search.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct array
+{
+    int A[20];
+    int size;
+    int length;
+};
+
+void input(struct array *arr)
+{
+    printf("\nEnter the %d elements in Array: ", arr->length);
+    for (int i = 0; i < arr->length; i++)
+    {
+        scanf("%d", &arr->A[i]);
+    }
+}
+
+int linear_search(struct array *arr, int key)
+{
+    for (int i = 0; i < arr->length; i++)
+    {
+        if (key == arr->A[i])
+        {
+            return i + 1; //just for user understanding
+        }
+    }
+    return -1;
+}
+int main()
+{
+    struct array arr;
+
+    int choice = 0;
+    int key;
+
+    arr.size = 20;
+    printf("\nEnter the length og Array: ");
+    scanf("%d", &arr.length);
+
+    input(&arr);
+
+    printf("\nDo you want to Search ?[1/0]");
+    scanf("%d", &choice);
+
+    if (choice == 1)
+    {
+        printf("\nEnter the Key:");
+        scanf("%d", &key);
+
+        printf("%d key is at %d index", key, linear_search(&arr, key));
+    }
+}
